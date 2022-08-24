f331caae3a5ebfba6d7475f304eb548c78c14f1e
Jade <Jade@gmail.com>
2022-08-23 22:59:13 -0300

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,28 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto_x';
-  @ViewChild('seccionFotos') secFotos: ElementRef | any;
-  public scrollToIntro() {
-    this.secFotos.nativeElement.scrollIntoView({
+  @ViewChild('seccionSkill') secFotosuno: ElementRef | any;
+  @ViewChild('seccionAbout') secFotosdos: ElementRef | any;
+  @ViewChild('seccionExperiencia') secFotostres: ElementRef | any;
+  scrollDos() {
+    this.secFotosuno.nativeElement.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
         inline: 'nearest'
     });
   }
-
+  scrollUno() {
+    this.secFotosdos.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
+  scrollTres() {
+    this.secFotostres.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
 }
