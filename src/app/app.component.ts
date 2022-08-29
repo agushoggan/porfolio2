2d1ebfa5c7f72819f4a28270deb78b6c04faeb92
Jade <Jade@gmail.com>
2022-08-29 16:37:01 -0300

    actualizacion2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,4 +39,16 @@ export class AppComponent {
         inline: 'nearest'
     });
   }
+  Instagram(){
+    window.open('https://www.instagram.com/ezequielbsso?r=nametag');
+  }
+  Github(){
+    window.open('https://github.com/');
+  }
+  Linkedin(){
+    window.open('https://www.linkedin.com/in/ezequiel-barragan-8463a51a6/');
+  }
+  Twitter(){
+    window.open('https://twitter.com/ezee_bsso03');
+  }
 }
